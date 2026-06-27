0da08cd71781962f3a09f3558240c0d59c034a93
Rakshitacnaik <rakshitanaik56@gmail.com>
2026-06-27 12:33:11 +0530

    updated project files

--- schemas/company.py
+++ schemas/company.py
@@ -1,10 +1,15 @@
 from pydantic import BaseModel
 from typing import Optional
 
-class CompanyCreate(BaseModel):
-    name: str
-    location: str
+class CompanyBase(BaseModel):
+    name:str
+    email:str
+    phone:str
 
-class CompanyUpdate(BaseModel):
+class CompanyCreate(CompanyBase):
+    pass 
+
+class CompanyUpdate(CompanyBase):
     name: Optional[str] = None
-    location: Optional[str] = None
+    email: Optional[str] = None
+    phone: Optional[str] = None
